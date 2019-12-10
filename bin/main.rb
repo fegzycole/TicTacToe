ef6897a46421ced0a1cbe8d45dd969da8facd395
kalavhan <47485749+kalavhan@users.noreply.github.com>
2019-12-10 13:50:29 -0700

    Added basic question for user

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 
-puts 'Hello world!'
-# player1 = X
-# player2 = O
 valid_options = %w[1 2 3 4 5 6 7 8 9]
-puts "Player 1 select a number from 1 to 9"
-option_selected = gets.chomp
-puts "run selection" unless valid_options.include? option_selected
+loop do
+  puts "Player 1 select a number from 1 to 9"
+  option_selected = gets.chomp
+  break if valid_options.include? option_selected
+end
 # print the 
+valid_options[option_selected] = "X"
+puts "#{valid_options[0]} | #{valid_options[1]} | #{valid_options[2]}"
+puts "---------"
+puts "#{valid_options[3]} | #{valid_options[4]} | #{valid_options[5]}"
+puts "---------"
+puts "#{valid_options[6]} | #{valid_options[7]} | #{valid_options[8]}"
 
-puts "1 | 2 | 3"
-puts "---------"
-puts "4 | 5 | 6"
-puts "---------"
-puts "7 | 8 | 9"
