76ed10afacadf14289d602611438335c2c481221
kalavhan <47485749+kalavhan@users.noreply.github.com>
2019-12-10 13:26:07 -0700

    added basic layout

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,16 @@
 #!/usr/bin/env ruby
 
 puts 'Hello world!'
+# player1 = X
+# player2 = O
+valid_options = %w[1 2 3 4 5 6 7 8 9]
+puts "Player 1 select a number from 1 to 9"
+option_selected = gets.chomp
+puts "run selection" unless valid_options.include? option_selected
+# print the 
+
+puts "1 | 2 | 3"
+puts "---------"
+puts "4 | 5 | 6"
+puts "---------"
+puts "7 | 8 | 9"
