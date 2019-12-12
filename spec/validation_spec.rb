b269334695a4b94776c0df8ba375443ca2def0ef
Iyara Ferguson <fergusoniyara@gmail.com>
2019-12-12 16:55:23 +0100

    Add tests for the validation class

---
+++ spec/validation_spec.rb
@@ -0,0 +1,28 @@
+require './lib/validation'
+
+describe Validation do
+  let(:validation) { Validation.new }
+
+  describe '#check_valid_input' do
+    it 'should return true if we enter a valid position' do
+      expect(validation.check_valid_input('2')).to eql(true)
+    end
+
+    it 'should return false if we enter an invalid position' do
+      expect(validation.check_valid_input('79')).to eql(false)
+    end
+
+    it 'should return false if we enter an alphabet' do
+      expect(validation.check_valid_input('S')).to eql(false)
+    end
+
+    it 'should return false if we enter an integer' do
+      expect(validation.check_valid_input(4)).to eql(false)
+    end
+
+    it 'should return false if we enter a position that has been selected' do
+      validation.update_selected_options('2')
+      expect(validation.check_valid_input('2')).to eql(false)
+    end
+  end
+end
